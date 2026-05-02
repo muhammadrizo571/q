e4160f4fa152fb05728584d82e91d21259e2c1c3
muhammadrizo571 <muhammadrizoa214@gmail.com>
2026-05-02 21:13:42 +0500

    dasiyui ulangan

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='max-w-[1200px] border-2 m-auto mt-[100px]'>
+
+      <div className="flex">
+
+         <ul className='capitalize flex justify-around gap-[50%] font-bold '>
+           <li>Home</li>
+           <li>about</li>
+           <li>contact</li>
+           <li>serve</li>
+         </ul>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
