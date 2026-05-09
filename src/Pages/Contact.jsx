857c336357c3539051b6f0f5baf4b3ee27eee5df
mahmudjonuktamov77-cmd <mahmudjonuktamov77@gmail.com>
2026-05-09 10:52:26 +0500

    1223
    bu swiper

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import {card} from './Map'
+import Swiper2 from '../Swiper1/Swiper2'
+
+
+// import Swiper from '../Swiper/Swiper'
 
 const Contact = () => {
   return (
     <div className=' max-[1440px] : h-auto border-2 '>
+  <div>
 
+<Swiper2/>
 
+  </div>
 
 
 </div>
